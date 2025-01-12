0d55ea134c4e1b1203e6d61342412d8818c39a2a
hansol <wkdqhrh00@naver.com>
2025-01-12 15:37:13 +0900

    start study

---
+++ src/main/java/com/hansol/jpabestpractices/JpaBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.hansol.jpabestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBestPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBestPracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hansol/jpabestpractices/JpaBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.jpabestpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBestPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
